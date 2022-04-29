5a0154f3a20369f13db0bf1a464c45c1a890b9a8
vtbano <vtbano@gmail.com>
2022-04-29 01:08:54 -0400

    add media queries to .gameboard,.block, .choose-block, .space, .space-button

--- src/App.js
+++ src/App.js
@@ -98,29 +98,7 @@ const App = () => {
             />
           </article>
         </section>
-        <footer>
-          {footerActive ? <Footer /> : ""}
-
-          {/* <span className="created-by">
-            Created by Vanessa Bano |
-            <a
-              href="https://github.com/vtbano/Pentago.git"
-              target="_blank"
-              alt="GitHub link to Pentago App"
-              id="githubLink"
-            >
-              GitHub |
-            </a>
-            <a
-              href="https://www.linkedin.com/in/vanessatbano/"
-              target="_blank"
-              alt="Vanessa Bano's LinkedIn profile"
-              id="LinkedIn"
-            >
-              LinkedIn
-            </a>
-          </span> */}
-        </footer>
+        <footer>{footerActive ? <Footer /> : ""}</footer>
       </body>
     </React.Fragment>
   );
